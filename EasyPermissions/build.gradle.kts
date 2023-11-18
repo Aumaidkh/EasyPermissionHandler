73e0ef701cabdab3b7666c4ef6746eafef605e82
AumaidKh <murtaza@fourthfrontier.com>
2023-11-18 20:45:51 +0530

    Examples Added

--- EasyPermissions/build.gradle.kts
+++ EasyPermissions/build.gradle.kts
@@ -54,7 +54,7 @@ afterEvaluate {
 
                 groupId = "com.github.aumaidkh"
                 artifactId = "easypermissions"
-                version = "1.0.7"
+                version = "1.0.8"
             }
         }
     }
